3ccd7b7526fc6e4afcc9113d1ad3732a61305996
truth <truth@gmail.com>
2023-11-20 13:47:01 -0800

    print_integer_err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError,ValueError):
-        print("Exception: {}".format(sys.exec_info()[1]),file-sys.stdder)
+        print("Exception: {}".format(sys.exec_info()[1]),file=sys.stdder)
         return (False)
 
