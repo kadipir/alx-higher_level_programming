5d59306816429cccbdd35f095408406c9aed73a3
truth <truth@gmail.com>
2023-11-20 14:03:11 -0800

    safe_function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ def safe_function(fct, *args):
         res = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e)),file = sys.stderr)
-        return none
+        return None
     else: 
         return res
 
