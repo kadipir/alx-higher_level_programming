d4c1667216d0eee0a7e5e39a027226d283c31262
truth <truth@gmail.com>
2023-11-20 14:00:44 -0800

    print_integer_err

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+    import sys
     try:
         res = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e)),file = sys.stderr)
         return none
     else: 
-        return (res)
+        return res
 
