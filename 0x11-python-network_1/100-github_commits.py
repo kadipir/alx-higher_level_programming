a65f8bb23ee840fd5fb8c838a39de9e91910d0cb
truth <truth@gmail.com>
2024-03-03 12:53:39 -0800

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”
-You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/
+"""
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)
- """
-if __name__ == "__main__":
-     from sys import argv
-     from requests import get
+"""
 
-     url = '"https://api.github.com/repos/{}/{}/commits".format(owner, repo)'
-     response = get(url)
-     json = response.json()
-     i = 0
 
-     repo = argv[1]
-     owner = argv[2]
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-     for element in json:
-         if i > 9:
-             break
-         sha = element.get('sha')
-         author = element.get('commit').get('author').get('name')
-         print('{}: {}'.format(sha, author))
-         i += 1
-     
+    repo = argv[1]
+    owner = argv[2]
+    i = 0
+
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = get(URL)
+    json = response.json()
+
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
