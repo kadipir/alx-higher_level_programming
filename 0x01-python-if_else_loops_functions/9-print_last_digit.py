f1f214b10d67f4b806e63ac5a50b45e74066d6f5
truther <trutherkadi.gnmail.com>
2024-05-15 11:41:53 -0700

    print_last_digit(number):

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_digit = number % -(10)
+        print(-(last_number), end="")
+    else:
+        last_digit = number % 10
+        print(last_number, end="")
+    return abs(last_number)
+   
+
