e3487aed19d10bc216c5ca56a794e103a53c12fb
truther <trutherkadi.gnmail.com>
2024-05-16 05:06:29 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,5 +9,5 @@ def print_matrix_integer(matrix=[[]]):
                     endspace = ' '
                 else:
                     endspace = ''
-                print("[:d]".format(matrix[row][item]), end=endspace)
+                print("{:d}".format(matrix[row][element]), end=endspace)
             print()
