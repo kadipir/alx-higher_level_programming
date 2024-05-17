a70c39862f9770baa5e07e686665b8ba960f1718
truther <trutherkadi.gnmail.com>
2024-05-17 05:50:55 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max = 0
+    for k, v in a_dictionary.items():
+        if v > max:
+            max = v
+    for k, v in a_dictionary.items():
+        if v == max:
+            return v
+
+  
