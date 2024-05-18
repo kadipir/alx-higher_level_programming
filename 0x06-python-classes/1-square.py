ef6b91e264bf76613f5cc304e3e903efc7a958d6
truther <trutherkadi.gnmail.com>
2024-05-18 11:24:19 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square module"""
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """constructor
+        Args:
+           size: length of side of square
+        """
+        self.__size = size
+
+
+
+
