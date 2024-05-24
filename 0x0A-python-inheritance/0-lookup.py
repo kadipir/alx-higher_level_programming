9b55321ca3935efc855abce520c421e5da749da2
truther <trutherkadi.gnmail.com>
2024-05-24 04:35:36 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0-lookup.py
+
+""" File name : 0-lookup.py
+    It is not allowed to import any module
+"""
+
+
+def lookup(obj):
+    """ A list object"""
+    return dir(obj)
